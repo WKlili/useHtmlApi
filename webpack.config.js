4c91a21d9933715c42f1bfc6cc0b71620fc6b035
yellow <18136225579@163.com>
2018-07-07 16:38:35 +0800

    test cssLoader Ok

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,15 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [{
+      // ./static/css/style.css
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader'
+      ]
+    }]
   }
 };
